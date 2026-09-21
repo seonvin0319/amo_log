a8351b0cd28f6d9f826e7d4fbed19dc5d4e87889
Seonvin Cho <pureum0319@gmail.com>
2026-09-21 10:39:19 +0900

    Move current IQL L_E+L2RMS runs off main into ablation.

--- scripts/log_layout.py
+++ scripts/log_layout.py
@@ -144,7 +144,7 @@ def classify(m, c):
         if number(c.get('beta_initial')) not in MAIN_BETAS:reasons.append('initial_beta_outside_main')
         lel2=c.get('execution_meta_loss')=='le_l2_rms'
         if lel2:
-            if fam!='iql_amo_lel2':reasons.append('execution_meta_lel2')
+            reasons.append('execution_meta_lel2')
         elif fam not in ('amo_bpi','iql_amo_jax_adroit_beta1_rho','lr1e3_beta_sweep'):
             reasons.append('method_variant:'+fam)
     elif method=='aspc' and c.get('l3_mode','aspc')!='aspc': reasons.append('l3_variant')
